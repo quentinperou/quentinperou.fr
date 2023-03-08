a2167e3580b27f89eab3a512910dc1935cc86f56
quentinperou <67630351+quentinperou@users.noreply.github.com>
2023-03-08 12:54:27 +0100

    update beta 1.1

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Code de mon portfolio : 
 https://quentinperou.fr/
 
-![home page](projets/web-portfolio-v2/apercu-portfolio-v2.jpg)
+![home page](images/apercu-portfolio-v2.jpg)
--- mentions-legales.php
+++ mentions-legales.php
@@ -1,1 +1,18 @@
-MAIS BIEN SUR
+Les mentions légales de quentinperou.fr
+
+C'est hébergé chez OVH
+
+Y a pas de cookies
+
+Y a pas de tracking
+
+Y a pas de pub
+
+Y a pas de partage de données
+
+J'ai fait simple
+
+le code ici : https://github.com/quentinperou/quentinperou.fr
+
+
+Je troll un peu, mais c'est quand même un peu vrai.
