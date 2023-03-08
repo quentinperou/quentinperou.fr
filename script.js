310e83c8e948dbbc74d8f555bc059537ec07eefe
quentinperou <67630351+quentinperou@users.noreply.github.com>
2023-03-08 12:53:29 +0100

    vérif url

--- script.js
+++ script.js
@@ -10,37 +10,36 @@
         /********** GESTION DU MENU BURGER **********/
         const menu = document.querySelector(".menu");
         const burgerMenu = document.querySelector(".burgerMenu");
-
         burgerMenu.addEventListener("click", function () {
             menu.classList.toggle("visible");
             this.classList.toggle("change");
         });
 
-        // document.getElementById('photoProfil').src = `images/photo_profil-${Math.floor(Math.random() * 4) + 1}-c.jpg`
+        /********** GESTION DES LIENS **********/
+        document.querySelectorAll(".menuElement, .websiteLogo a, [href='#projets']").forEach(function (element) {
+            element.addEventListener("click", function (elem) {
+                if (menu.classList.contains("visible")) {
+                    menu.classList.toggle("visible");
+                    burgerMenu.classList.toggle("change");
+                }
 
-        // document.querySelectorAll(".menuElement").forEach(function (element) {
-        //     element.addEventListener("click", function (elem) {
-        //         if (menu.classList.contains("visible")) {
-        //             menu.classList.toggle("visible");
-        //             burgerMenu.classList.toggle("change");
-        //         }
-        //         /** Scroll dans le menu **/
-        //         if (this.getAttribute("href")[0] == '#') {
-        //             elem.preventDefault();
-        //             if (this.getAttribute("href") == '#')
-        //                 document.querySelector("header").scrollIntoView();
-        //             else
-        //                 document.querySelector(`${this.getAttribute("href")}`).scrollIntoView();
-        //         }
+                if (this.getAttribute("href")[0] == '#') {
+                    elem.preventDefault();
+                    let url = window.location.href.split('/');
+                    console.log(window.location.pathname, url);
+                    console.log(url[url.length - 1])
+                    if (this.getAttribute("href") == '#' && url[url.length - 1] == "")
+                        document.querySelector("body").scrollIntoView();
+                    else
+                        document.querySelector(`${this.getAttribute("href")}`).scrollIntoView();
+                }
 
-        //     });
-        // });
-
-        // document.querySelector(".goEnHaut").addEventListener("click", function (evt) {
-        //     evt.preventDefault();
-        //     document.querySelector("header").scrollIntoView();
-        // });
+            });
+        });
+        document.querySelector(".goEnHaut").addEventListener("click", function (evt) {
+            evt.preventDefault();
+            document.querySelector("body").scrollIntoView();
+        });
 
     });
-
 }());
