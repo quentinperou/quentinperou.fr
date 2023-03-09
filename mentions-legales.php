45a5112699e30b2d73a38abfb923c6582e834832
quentinperou <67630351+quentinperou@users.noreply.github.com>
2023-03-09 21:15:02 +0100

    maj mention legale

--- mentions-legales.php
+++ mentions-legales.php
@@ -19,7 +19,7 @@ session_start();
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:creator" content="@quentinperou" />
     <meta property="og:url" content="https://quentinperou.fr" />
-    <meta property="og:title" content="Quentin Pérou | Photographie" />
+    <meta property="og:title" content="Quentin Pérou | Mentions Légales" />
     <meta property="og:description" content="Portfolio de Quentin Pérou, photographe et vidéaste." />
     <meta property="og:image" content="https://quentinperou.fr/images/photo_profil-4-c.jpg">
 
@@ -47,7 +47,7 @@ session_start();
             </div>
             <nav class="menu">
                 <a href="./" class="menuElement">Accueil</a>
-                <a href="./photographie/" class="menuElement selected">Photographie</a>
+                <a href="./photographie/" class="menuElement">Photographie</a>
                 <a href="./video/" class="menuElement">Vidéo</a>
                 <!-- <a href="#contact" class="menuElement">Contact</a> -->
             </nav>
@@ -62,31 +62,62 @@ session_start();
     <main>
 
         <section>
-            <p>Les mentions légales de <a href="https://quentinperou.fr">quentinperou.fr</a></p>
-            <p>C'est hébergé chez OVH</p>
-            <p>Y a pas de cookies</p>
-            <p>Y a pas de tracking</p>
-            <p>Y a pas de pub</p>
-            <p>Y a pas de partage de données</p>
-            <p>J'ai fait simple</p>
-            <p>le code ici : <a href="https://github.com/quentinperou/quentinperou.fr">https://github.com/quentinperou/quentinperou.fr</a></p>
+            <h2>Mentions Légales</h2>
+            <div style="font-size: 16px;">
+                <style>
+                    h4 {
+                        margin-bottom: 0;
+                    }
 
-            <p>Je troll un peu, mais c'est quand même un peu vrai.</p>
+                    p {
+                        margin-top: 0;
+                    }
+                </style>
+                <p>
+                    En vigueur au 01/04/2020 Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des Utilisateurs du site quentinperou.fr les présentes mentions légales.
+                    <br>
+                    La connexion et la navigation sur le site quentinperou.fr par l’Utilisateur implique acceptation intégrale et sans réserve des présentes mentions légales.
+                    <br>
+                    Ces dernières sont accessibles sur le site à la rubrique « Mentions légales ».
+                </p>
+
+                <h4>ARTICLE 1 : L’ÉDITEUR</h4>
+                <p>L’édition et la direction de la publication du site www.quentinperou.fr est assurée par Quentin PEROU, domicilié à 44300 NANTES, dont l'adresse e-mail quentinperou.contact@gmail.com.</p>
+
+                <h4>ARTICLE 2 : L’HÉBERGEUR</h4>
+                <p>L'hébergeur du site quentinperou.fr est la Société OVH, dont le siège social est situé au 2 rue Kellermann – BP 80157 59053 ROUBAIX CEDEX 1.</p>
+
+                <h4>ARTICLE 3 : ACCÈS AU SITE</h4>
+                <p>
+                    Le site est accessible par tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant d’une nécessité de maintenance.
+                    <br>
+                    En cas de modification, interruption ou suspension des services le site quentinperou.fr ne saurait être tenu responsable.
+                </p>
+
+                <h4>ARTICLE 4 : COLLECTE DES DONNÉES</h4>
+                <p>Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans la mesure où il ne collecte aucune donnée concernant les utilisateurs.</p>
+
+                <h4>ARTICLE 5 : COOKIES</h4>
+                <p>
+                    L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.
+                    En naviguant sur le site, il les accepte.
+                    <br>
+                    Un cookie est un élément qui ne permet pas d’identifier l’Utilisateur mais sert à enregistrer des informations relatives à la navigation de celui-ci sur le site Internet. L’Utilisateur pourra désactiver ce cookie par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.
+                </p>
+
+                <h4>ARTICLE 6 : PROPRIÉTÉ INTELLECTUELLE</h4>
+                <p>Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du site quentinperou.fr , sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.</p>
+
+
+                <br>
+                <p>Le code source du site est disponible sur GitHub : <a href="https://github.com/quentinperou/quentinperou.fr">https://github.com/quentinperou/quentinperou.fr</a></p>
+
+            </div>
         </section>
-
-
 
     </main>
 
     <footer>
-        <a href="#">
-            <div class="goEnHaut" title="Revenir en haut de la page">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="18px" height="18px">
-                    <path d="M0 0h24v24H0V0z" fill="none" />
-                    <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6 1.41 1.41z" />
-                </svg>
-            </div>
-        </a>
         <p>© 2023 Quentin Pérou </p>
     </footer>
 
