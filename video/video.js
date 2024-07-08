a2fdee1b330b5a992b39d4e934090bcf62c625fc
quentinperou <67630351+quentinperou@users.noreply.github.com>
2024-07-08 19:57:31 +0200

    update youtube link on video page

--- video/video.js
+++ video/video.js
@@ -9,10 +9,10 @@
             let videoLink = btn.href.split("/");
             // console.log("videoLink", videoLink);
 
-            // Si c'est une video youtube et que le site n'est pas ouvert sur un mobile
+            // Si c'est une video youtube /et que le site n'est pas ouvert sur un mobile
             if (videoLink[2].startsWith("youtu") /*&& window.innerWidth > 768*/ ) {
                 evt.preventDefault();
-                videoViewer.querySelector('iframe').src = `https://www.youtube-nocookie.com/embed/${videoLink[videoLink.length - 1]}?showinfo=0&color=white&rel=0`;
+                videoViewer.querySelector('iframe').src = `https://www.youtube-nocookie.com/embed/${videoLink[videoLink.length - 1]}?showinfo=0&color=white&rel=0&autoplay=1&vq=hd1080`;
                 videoViewer.querySelector('iframe').removeAttribute("style");
                 videoViewer.classList.add("visible");
             } else {
